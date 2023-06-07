54777bf771fa93c1c810043b09f16a162e3b5f33
patr4519 <jero96@mail.ru>
2023-06-07 20:54:53 +0300

    Eddited header

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import './index.scss';
-import { Header } from './components/Header';
-import { Footer } from './components/Footer';
-import { Generator } from './components/Generator';
+import React from "react";
+import "./index.scss";
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
+import { Generator } from "./components/Generator";
 
 function App() {
   return (
     <div className="App">
-        <Header />
-        <Generator />
-        <Footer />
+      <Header />
+      <Generator />
+      <Footer />
     </div>
   );
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
-import styles from './Header.module.scss'
+import React from "react";
+import styles from "./Header.module.scss";
 
 export const Header = () => {
-    return (
-        <header className={styles.header}>
-            Header
-        </header>
-    )
-}
+  return (
+    <header className={styles.header}>
+      <div className={styles.title}>
+        <div className={styles.icon}>
+          img
+        </div>
+        <div className={styles.text}>Password generator</div>
+      </div>
+    </header>
+  );
+};
