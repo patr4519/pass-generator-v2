795bcbe37d1629b6f0ead415784a5be0cd811722
patr4519 <jero96@mail.ru>
2023-06-07 19:02:18 +0300

    Added template for Generator component

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './index.scss';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
+import { Generator } from './components/Generator';
 
 function App() {
   return (
     <div className="App">
         <Header />
+        <Generator />
         <Footer />
     </div>
   );
---
+++ src/components/Generator/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Generator.module.scss'
+
+export const Generator = () => {
+    return (
+        <div className={styles.generator}>
+            Generator
+        </div>
+    )
+}
